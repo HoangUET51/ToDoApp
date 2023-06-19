92eb83e97cc9b9489966453be824524218202337
hoang <vuvanhoanguet51@gmail.com>
2023-06-20 00:00:48 +0700

    setup bloc

---
+++ lib/view/todo_list/todo_list_bloc.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:to_do_list/view/todo_list/todo_list_event.dart';
+import 'package:to_do_list/view/todo_list/todo_list_state.dart';
+
+class TodoListBloc extends Bloc<TodoListEvent, ToDoListState> {
+  TodoListBloc(super.initialState);
+
+
+}
